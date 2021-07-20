747f70183299f5d2a89f0c66bebafa319a8d4dd3
tarun2506 <tarunsharma8920@gmail.com>
2021-07-20 23:29:20 +0530

    final

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "../styles/Header.css";
+import icon from "./image.png";
 
 function Header() {
   return (
     <div className="container-header">
       <nav className="nav">
-        <img src="https://img.icons8.com/ios/60/000000/virus.png" alt="img" />
+        <img src={icon} className="logo-img" height="70px" width="70px" />
         <h1 className="logo">Covid19 Trackr</h1>
       </nav>
     </div>
--- src/Components/Tracker.js
+++ src/Components/Tracker.js
@@ -6,14 +6,13 @@ function Tracker() {
   // States:
   const [input, setInput] = useState("");
   const [apiData, setApiData] = useState(null);
+  const [inputError, setInputError] = useState("");
   useEffect(() => {
     // Make an api req:
     axios
       .get(`https://disease.sh/v3/covid-19/countries/aka?strict=true`)
       .then((response) => {
-        console.log(response);
         setApiData(response.data);
-        console.log(apiData);
       })
       .catch((err) => {
         console.log(err.message);
@@ -31,9 +30,10 @@ function Tracker() {
         .get(`https://disease.sh/v3/covid-19/countries/${input}?strict=true`)
         .then((response) => {
           setApiData(response.data);
+          setInputError("");
         })
         .catch((err) => {
-          console.log(err.message);
+          setInputError("Check the Spelling...kiddo!");
         });
     } else {
       console.log("no input");
@@ -41,32 +41,47 @@ function Tracker() {
   };
 
   return (
-    <div className="container-tracker">
+    <>
       <div className="search">
-        <label>Country Name: </label>
-        <input
-          type="text"
-          className="Country-search"
-          required
-          onChange={handleInputValue}
-        />
-      </div>
-      <button type="submit" className="submit-btn" onClick={handleApiReq}>
-        Search
-      </button>
-      {apiData && (
-        <div>
-          <div className="tracker-info">
-            <h1>Population: {apiData.population}</h1>
-            <h2>Active cases - {apiData.active}</h2>
-            <h2>Total cases - {apiData.cases}</h2>
-            <h2>Total Deaths - {apiData.deaths}</h2>
-            <h2>Recovered: {apiData.recovered}</h2>
-            <img src={apiData.countryInfo.flag} alt="flag" />
-          </div>
+        <div className="input-label">
+          <label>Country Name ➡</label>
+          <input
+            type="text"
+            className="Country-search"
+            required
+            onChange={handleInputValue}
+          />
         </div>
+        <button type="submit" className="submit-btn" onClick={handleApiReq}>
+          Search
+        </button>
+      </div>
+      {apiData && (
+        <>
+          <div className="tracker-info">
+            <img src={apiData.countryInfo.flag} alt="flag" />
+            <div className="active-cases try">
+              <h2>Active: &nbsp;</h2>
+              <p>{apiData.active}</p>
+            </div>
+            <div className="total-cases try">
+              <h2>Total: &nbsp;</h2>
+              <p>{apiData.cases}</p>
+            </div>
+            <div className="total-deaths try">
+              <h2>Deaths: &nbsp;</h2>
+              <p>{apiData.deaths}</p>
+            </div>
+            <div className="Recovered try">
+              <h2>Recovered: &nbsp;</h2>
+              <p>{apiData.recovered}</p>
+            </div>
+          </div>
+        </>
       )}
-    </div>
+
+      {inputError && <div className="error-div">{inputError}</div>}
+    </>
   );
 }
 
