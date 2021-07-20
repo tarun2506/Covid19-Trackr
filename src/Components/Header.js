675c330401abc99e51115b53091b8e51ff3612fd
tarun2506 <tarunsharma8920@gmail.com>
2021-07-20 23:43:16 +0530

    cscs

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,7 +6,13 @@ function Header() {
   return (
     <div className="container-header">
       <nav className="nav">
-        <img src={icon} className="logo-img" height="70px" width="70px" />
+        <img
+          src={icon}
+          className="logo-img"
+          height="70px"
+          width="70px"
+          alt="sds"
+        />
         <h1 className="logo">Covid19 Trackr</h1>
       </nav>
     </div>
